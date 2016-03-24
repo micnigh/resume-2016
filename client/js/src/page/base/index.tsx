e9332fdbae0d39c128c00fbae4b068656d23b71b
Michael Nigh <mic.nigh@gmail.com>
2016-03-23 20:18:57 -0700

    add tooltips to page

--- client/js/src/page/base/index.tsx
+++ client/js/src/page/base/index.tsx
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
+import $ from "jquery";
 
 export class Base extends Component<any, any> {
+  componentDidMount () {
+    $("[title]", this.refs.page).tooltip({
+      container: `body`,
+      placement: (tip, e) => {
+        return $(e).data("tooltip-placement") || "top";
+      },
+    });
+  }
+
   render() {
     return (
-      <div className="page">
+      <div className="page" ref="page">
         <div className="content container">
           <div className="secondary-font">
             { this.props.children }
