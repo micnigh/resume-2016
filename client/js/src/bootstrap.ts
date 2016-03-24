385b5e8cc55f2c8766eb91350c154fd44e46bdaa
Michael Nigh <mic.nigh@gmail.com>
2016-03-23 20:18:42 -0700

    ensure jquery and bootstrap are loaded in app

--- client/js/src/bootstrap.ts
+++ client/js/src/bootstrap.ts
@@ -1,5 +1,8 @@
 import "regenerator/runtime";
 
+import "jquery";
+import "bootstrap";
+
 if (process.env.NODE_ENV !== "production") {
   if (process.env.BROWSER_SYNC_SNIPPETS !== "undefined") {
     (<string[]>process.env.BROWSER_SYNC_SNIPPETS).forEach(s => {
