1841c2ab69050e98a4b415e0232bdda2892cbcfe
Michael Nigh <mic.nigh@gmail.com>
2016-03-27 09:50:07 -0700

    browsersync now works on all connected devices

--- client/js/src/bootstrap.ts
+++ client/js/src/bootstrap.ts
@@ -5,8 +5,10 @@ import "bootstrap";
 
 if (process.env.NODE_ENV !== "production") {
   if (process.env.BROWSER_SYNC_SNIPPETS !== "undefined") {
-    (<string[]>process.env.BROWSER_SYNC_SNIPPETS).forEach(s => {
-      document.write(`<script src="${s}"></script>`);
+    (<{ port: number, path: string}[]>process.env.BROWSER_SYNC_SNIPPETS).forEach(s => {
+      let { protocol, hostname } = window.location;
+      let { port, path } = s;
+      document.write(`<script src="${protocol}//${hostname}:${port}${path}"></script>`);
     });
   }
 }
